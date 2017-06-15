b81cdfc5992d14cec797f7e31912b67af3427843
Artemiy Rodionov <artemiy.rodionov@gmail.com>
2017-06-16 00:07:51 +0300

    Add deploy sccript

---
+++ fabfile.py
@@ -0,0 +1,18 @@
+from fabric.api import *
+
+
+env.use_ssh_config = True
+env.hosts = ["snalg1"]
+
+
+folder = '/home/snoopt/summer'
+
+
+
+def deploy():
+    local('git archive -o /tmp/latest.zip HEAD')
+    with cd(folder):
+        put('/tmp/latest.zip', folder)
+        run('unzip latest.zip')
+    sudo('supervisorctl restart summer_bot')
+    local('rm /tmp/latest.zip')
